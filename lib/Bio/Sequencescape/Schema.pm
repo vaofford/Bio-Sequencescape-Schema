583736e578e0eecdaa8133d9e8158f0d0e408aa0
John Tate <jt6@sanger.ac.uk>
2016-01-15 15:07:59 +0000

    Set up basic dist.ini for dzil

--- lib/Bio/Sequencescape/Schema.pm
+++ lib/Bio/Sequencescape/Schema.pm
@@ -14,6 +14,7 @@ __PACKAGE__->load_namespaces;
 # Created by DBIx::Class::Schema::Loader v0.07042 @ 2016-01-07 16:49:50
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:IoCoXlxSu27HlnGEBfTY2Q
 
+# ABSTRACT: DBIC schema for the sequencescape warehouse
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
